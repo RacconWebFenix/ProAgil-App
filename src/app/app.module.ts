5e362257de7b88cacc565151484952797a9f4e74
Marcos Domingues <dfenixweb@gmail.com>
2020-11-18 09:57:57 -0300

    Adicionado Projeto Angular SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; //Fazer importação do ClientModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule //Importado do '@angular/common/http'
   ],
   providers: [],
   bootstrap: [AppComponent]
